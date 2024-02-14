d8a0497d80b4f86196acaf804dc1fe3c8c67b8c1
alonsofdez23 <alonsobs23@gmail.com>
2024-02-14 23:19:54 +0100

    change ORM to eclipselink

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,23 @@
 import jakarta.persistence.*;
+import modelo.entidad.Autor;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory factoria = Persistence.createEntityManagerFactory("a3JPA");
         EntityManager em = factoria.createEntityManager();
+
+        Autor autor = new Autor();
+        autor.setNombre("Alonso");
+        autor.setApellidos(" Fernández Vidal");
+        autor.setFechaNacimiento(LocalDate.parse("1990-03-06"));
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        em.persist(autor);
+        et.commit();
+
+        System.out.println("Autor creado");
     }
 }
